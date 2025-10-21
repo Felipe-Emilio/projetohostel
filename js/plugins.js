dd3cf152cfb4c30d25c8992d5736150c38f4c923
Felipe-Emilio-Quintela <98416871+Felipe-Emilio@users.noreply.github.com>
2025-10-21 19:24:44 -0300

    21/10/2025

--- js/plugins.js
+++ js/plugins.js
@@ -5,9 +5,6 @@
  * More info:
  * http://lokeshdhakar.com/projects/lightbox2/
  *
- * Copyright 2007, 2015 Lokesh Dhakar
- * Released under the MIT license
- * https://github.com/lokesh/lightbox2/blob/master/LICENSE
  */
  
 // Uses Node, AMD or browser globals to create a module.
